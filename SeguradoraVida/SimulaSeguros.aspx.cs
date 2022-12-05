5cb07be3d3825fb0e95eac8faed1e5ae014ce43c
Gabriel dos santos vieira <gabrielsantosv2@hotmail.com>
2022-12-05 18:42:41 -0300

    SimulaSeguro: Verificações do radio Button

--- SeguradoraVida/SimulaSeguros.aspx.cs
+++ SeguradoraVida/SimulaSeguros.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SeguradoraVida.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,18 +13,62 @@ namespace SeguradoraVida
         protected void Page_Load(object sender, EventArgs e)
         {
 
-              
+
 
         }
 
-        protected void btnEnviar_Click(object sender , EventArgs e)
+        protected void btnEnviar_Click(object sender, EventArgs e)
         {
             string nome = txtNome.Text;
             string email = txtEmail.Text;
             string telefone = txtTelefone.Text;
             string cpf = txtCpf.Text;
             string dataNascimento = txtData.Text;
+            string sexo = "";
+            string seguro = "";
 
+            
+            if (RadMasculino.Checked)
+            {
+                sexo = "Masculino";
+            }
+            else if (RadFeminino.Checked)
+            {
+                sexo = "Feminino";
+            }
+
+            if (RadInvalidez.Checked)
+            {
+                seguro = "Invalidez";
+            }
+            else if (RadMorteAcidental.Checked)
+            {
+                seguro = "Morte Acidental";
+            }
+            else if(RadDoencaHereditaria.Checked)
+            {
+                seguro = "Doença Hereditária";
+            }
+
+            
+
+
+            if (VerificaCampos())
+            {
+                Simular sm = new Simular(nome, dataNascimento, email, telefone, cpf, sexo, seguro);
+            }
+
+        }
+
+        private bool VerificaCampos()
+        {
+            if (txtCpf.Text == "" || txtData.Text == "" || txtEmail.Text == "" || txtNome.Text == "" || txtTelefone.Text == "")
+            {
+                Response.Write("<script>alert('Há campos que não foram preenchidos!');</script>");
+
+                return false;
+            }
+            return true;
         }
     }
 }
